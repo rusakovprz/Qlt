a6ea509d3a98a9f34c98b8e5aecf17d45c70d9f3
rusakovprz <rusakovprz@rambler.ru>
2014-12-06 22:28:24 +0300

    minor fix ref #12.

--- src/qltgplogger.h
+++ src/qltgplogger.h
@@ -1,8 +1,8 @@
 /*
-	File: qltgplogger.h
+  File: qltgplogger.h
 
-	class QltGpLogger - gnuplot logger, агрегатор данных подготавливающий и 
-	записывающий данные в формате пригодном для визуализации с помощю gnuplot. 
+  class QltGpLogger - gnuplot logger, агрегатор данных подготавливающий и 
+  записывающий данные в формате пригодном для визуализации с помощю gnuplot. 
 
 */
 
@@ -19,23 +19,23 @@ public:
   enum Mode { GeneralMode, GisMode };
   enum LabelType {First, Graph, Screen};
 
-	QltGpLogger(Mode mode = GeneralMode);
+  QltGpLogger(Mode mode = GeneralMode);
 
-	bool addColumn(QStringList column, QString label, bool axisY2 = false);
+  bool addColumn(QStringList column, QString label, bool axisY2 = false);
 	
-	bool addPolygon(QStringList lat, QStringList lon, QString label);
-	bool addTrack(QStringList lat, QStringList lon, QString label);
+  bool addPolygon(QStringList lat, QStringList lon, QString label);
+  bool addTrack(QStringList lat, QStringList lon, QString label);
 
-	void setImageMode(QString size = "800,600");
-	void setGuiMode();
+  void setImageMode(QString size = "800,600");
+  void setGuiMode();
 
-	bool toFile(QString fileName = "gpdata", bool genIndex = true);
+  bool toFile(QString fileName = "gpdata", bool genIndex = true);
 
-	QString errorString() { return errorString_; };
+  QString errorString() { return errorString_; };
 
-	void setTitleName(QString name);
-	void setXLabelName(QString name);
-	void setYLabelName(QString name);
+  void setTitleName(QString name);
+  void setXLabelName(QString name);
+  void setYLabelName(QString name);
   void setY2LabelName(QString name);
 
   void addLabel(LabelType type, double x, double y, QString text, QString color = "#000000");
@@ -56,34 +56,34 @@ private:
 
   Mode mode_;
     
-	QList<QStringList> container_;
-	QStringList labels_;
-	QList<bool> axisY2Flag_;
+  QList<QStringList> container_;
+  QStringList labels_;
+  QList<bool> axisY2Flag_;
 	
-	double y1_min_;
-	double y1_max_;
-	double y2_min_;
-	double y2_max_;
+  double y1_min_;
+  double y1_max_;
+  double y2_min_;
+  double y2_max_;
 	
-	QList<QStringList> polygonsLat_;
-	QList<QStringList> polygonsLon_;
-	QStringList polygonsLabels_;
+  QList<QStringList> polygonsLat_;
+  QList<QStringList> polygonsLon_;
+  QStringList polygonsLabels_;
 
- 	QList<QStringList> tracksLat_;
-	QList<QStringList> tracksLon_;
-	QStringList tracksLabels_;
+  QList<QStringList> tracksLat_;
+  QList<QStringList> tracksLon_;
+  QStringList tracksLabels_;
 
-	int lenColumn_;
-	QString errorString_;
+  int lenColumn_;
+  QString errorString_;
 
-	QString fileName_;
-	bool toImageFlag_;
-	QString sizeImage_;
+  QString fileName_;
+  bool toImageFlag_;
+  QString sizeImage_;
 
-	QString titleName_;
-	QString xLabelName_;
-	QString yLabelName_;
-	QString y2LabelName_; 
+  QString titleName_;
+  QString xLabelName_;
+  QString yLabelName_;
+  QString y2LabelName_; 
 
   QStringList firstLabels_;
   QStringList graphLabels_;
